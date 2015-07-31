b446e2680194a20188ab35cab525a9ea46b1e6dc
zoluxa <zoluxa@gmail.com>
2015-07-31 05:38:57 -0700

    Push

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Test
-Ruby projects
+# Calendar
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test::Application.config.secret_token = 'dc7f8ca40b67bfcf2f058340b708f3e429728a29d7bcb9f6916d8efb46bbf87457302bac6a27d9ec4e2fe72416b370cee21f94515f30d5fba016678b585a9b7f'
